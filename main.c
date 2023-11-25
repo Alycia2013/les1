ad7273ea90f5db56d682fa6c3818696c1fd7993e
Alycia2013 <151991834+Alycia2013@users.noreply.github.com>
2023-11-25 12:38:17 +0000

    meerdere bewerkingen

--- main.c
+++ main.c
@@ -1,15 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int vraag_cijfer() {
+int vraag_cijfer()
+{
     int cijfer;
     printf("geef een cijfer: ");
-    scanf("%d",  &cijfer);
+    scanf("%d", &cijfer);
     return cijfer;
 }
 
-void main() {
+int main()
+{
     int cijfer_1, cijfer_2;
+    char bewerking;
     cijfer_1 = vraag_cijfer();
     cijfer_2 = vraag_cijfer();
-    printf("de som is: %d\n", cijfer_1 + cijfer_2);
+    printf("welke bewerking wil je?: ");
+    scanf(" %c", &bewerking);
+
+    int uitkomst;
+    switch (bewerking)
+    {
+    case '*':
+        uitkomst = cijfer_1 * cijfer_2;
+        break;
+    case '+':
+        uitkomst = cijfer_1 + cijfer_2;
+        break;
+    case '/':
+        uitkomst = cijfer_1 / cijfer_2;
+        break;
+    case '-':
+        uitkomst = cijfer_1 - cijfer_2;
+        break;
+    default:
+        printf("sorry ik begrijp je niet\n");
+        return 1;
+    }
+    printf("de uitomst is: %d\n", uitkomst);
+    return 0;
 }

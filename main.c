29bfcfc2772b9ddb3d435c093c9ea04d15c12c8b
Alycia2013 <151991834+Alycia2013@users.noreply.github.com>
2023-11-26 09:01:16 +0000

    ja of nee

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int vraag_cijfer()
 {
     int cijfer;
     printf("geef een cijfer: ");
-    scanf("%d", &cijfer);
+    scanf(" %d", &cijfer);
     return cijfer;
 }
 
@@ -12,31 +12,35 @@ int main()
 {
     int cijfer_1, cijfer_2;
     char bewerking;
-    cijfer_1 = vraag_cijfer();
-    cijfer_2 = vraag_cijfer();
-    printf("welke bewerking wil je?: ");
-    scanf(" %c", &bewerking);
+    char nog_eens='j';
+    while (nog_eens == 'j' || nog_eens == 'J') {
+        cijfer_1 = vraag_cijfer();
+        cijfer_2 = vraag_cijfer();
+        printf("welke bewerking wil je?: ");
+        scanf(" %c", &bewerking);
 
-    int uitkomst;
-    switch (bewerking) {
-    case '*':
-        uitkomst = cijfer_1 * cijfer_2;
-        break;
-    case '+':
-        uitkomst = cijfer_1 + cijfer_2;
-        break;
-    case '/':
-        uitkomst = cijfer_1 / cijfer_2;
-        break;
-    case '-':
-        uitkomst = cijfer_1 - cijfer_2;
-        break;
-    default:
-        printf("sorry ik begrijp je niet\n");
-        return 1;
+        int uitkomst;
+        switch (bewerking) {
+        case '*':
+            uitkomst = cijfer_1 * cijfer_2;
+            break;
+        case '+':
+            uitkomst = cijfer_1 + cijfer_2;
+            break;
+        case '/':
+            uitkomst = cijfer_1 / cijfer_2;
+            break;
+        case '-':
+            uitkomst = cijfer_1 - cijfer_2;
+            break;
+        default:
+            printf("sorry ik begrijp je niet\n");
+            return 1;
+        }
+        printf("de uitomst is: %d\n", uitkomst);
+        printf("wil nog eens? [j/n]: ");
+        scanf(" %c", &nog_eens);
     }
-    printf("goed gedaan\n");
-    printf("de uitomst is: %d\n", uitkomst);
     return 0;  
 } 
 
